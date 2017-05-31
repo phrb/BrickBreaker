6e3c657e6a7a349a1e610b9802d7d2956de27706
Pedro Bruel <pedro.bruel@gmail.com>
2017-05-31 17:46:58 -0300

    Change condition verification.
    Seems simpler this way since it bundles comparisions in the same 'if'.

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -13,9 +13,7 @@ func _fixed_process(delta):
 	set_pos(Vector2(mouse_x, y))
 	
 func _input(event):
-	if ball_presence: return
-
-	if event.type == InputEvent.MOUSE_BUTTON and event.is_pressed():
+	if !ball_presence and (event.type == InputEvent.MOUSE_BUTTON and event.is_pressed()):
 		var ball = ball_scene.instance()
 		ball.set_pos(get_pos() - Vector2(0, 32))
 		get_tree().get_root().add_child(ball)
