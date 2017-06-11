4cd9bf4e7e690faccf94cdd96225a7b6a421bf3f
Lucas Kanashiro <kanashiro@debian.org>
2017-06-11 15:34:00 -0300

    update game over and winner scenes with new graphics

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -6,16 +6,16 @@ extends Node2D
 
 func _ready():
 	get_node("../World/Paddle").game_over = true
-	get_node("high_score").set_text(str(get_node("/root/World").high_score))
-	get_node("your_score").set_text(str(get_node("/root/World").score))
+	var high_score = get_node("/root/World").high_score
+	var your_score = get_node("/root/World").score
+	if your_score < high_score:
+		get_node("high_score_msg").hide()
+	get_node("high_score").set_text(str(high_score))
+	get_node("your_score").set_text(str(your_score))
 	
 	get_node("play").connect("pressed", self, "replay_game")
-	get_node("menu").connect("pressed", self, "go_to_menu")
 
 func replay_game():
 	queue_free()
 	get_node("/root/World").remove_balls()
 	get_tree().change_scene("res://scenes/level1.xml")
-
-func go_to_menu():
-	print("You should start menu scene here")
--- scripts/winner.gd
+++ scripts/winner.gd
@@ -10,12 +10,8 @@ func _ready():
 	get_node("your_score").set_text(str(your_score))
 	
 	get_node("play").connect("pressed", self, "replay_game")
-	get_node("menu").connect("pressed", self, "go_to_menu")
 
 func replay_game():
 	get_node("/root/World").remove_balls()
 	get_tree().change_scene("res://scenes/level1.xml")
 	queue_free()
-
-func go_to_menu():
-	print("You should start menu scene here")
